6e05925d36a61942da3bfdc7dbe0ae23683ba78e
Lars <146138654+jaavalover@users.noreply.github.com>
2023-10-05 00:00:58 +0200

    Main.java
    I made the encoded/decoded text more recognizable in the console by adding "Base64 en/decoded: " in front of the encode/decode results.
    
    The program no longer exits when an encoding or decoding process has been successfully completed, it repeats the program infinitely until you exit it manually.

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+package me.lars;
+
 import java.util.Base64;
 import java.util.Scanner;
 
@@ -7,41 +9,42 @@ public class Main {
     }
 
     static void start(){
-            System.out.println();
-            System.out.print("\u001B[35m"); // ANSI PURPLE
-            System.out.print("Base64 Encoder & Decodier");
-            System.out.print("\u001B[0m"); // ANSI RESET
-            System.out.println(" by lars.dev ");
-            System.out.println("Please select an option:");
-            System.out.println();
-            System.out.println("[1] Encoding");
-            System.out.println("[2] Decoding");
-            System.out.println("[3] Quit");
-            System.out.println();
+        System.out.println();
+        System.out.print("\u001B[35m"); // ANSI PURPLE
+        System.out.print("Base64 Encoder & Decodier");
+        System.out.print("\u001B[0m"); // ANSI RESET
+        System.out.println(" by lars.dev ");
+        System.out.println("Please select an option:");
+        System.out.println();
+        System.out.println("[1] Encoding");
+        System.out.println("[2] Decoding");
+        System.out.println("[3] Quit");
+        System.out.println();
 
-            Scanner scanner = new Scanner(System.in);
-            String selected = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String selected = scanner.nextLine();
 
-            switch (selected) {
-                case "1":
-                    encode();
-                    break;
-                case "2":
-                    decode();
-                    break;
-                case "3":
-                    quit();
-                    break;
-                default:
-                    System.out.print("\u001B[31m"); // ANSI RED
-                    System.out.println("Invalid Option");
-                    System.out.print("\u001B[0m"); // ANSI RESET
-                    start();
-            }
+        switch (selected) {
+            case "1":
+                encode();
+                break;
+            case "2":
+                decode();
+                break;
+            case "3":
+                quit();
+                break;
+            default:
+                System.out.print("\u001B[31m"); // ANSI RED
+                System.out.println("Invalid Option");
+                System.out.print("\u001B[0m"); // ANSI RESET
+                start();
+        }
     }
 
     static void encode(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println();
         System.out.println("Base64 Encoder");
         System.out.println("Enter text to encode:");
         System.out.println();
@@ -50,12 +53,13 @@ public class Main {
 
         byte[] encodedBytes = Base64.getEncoder().encode(text.getBytes());
         String encodedText = new String(encodedBytes);
-        System.out.println(encodedText);
-        scanner.close();
+        System.out.println("Base64 encoded: " + encodedText);
+        start();
     }
 
     static void decode() {
         Scanner scanner = new Scanner(System.in);
+        System.out.println();
         System.out.println("Base64 Decoder");
         System.out.println("Enter text to decode:");
         System.out.println();
@@ -64,8 +68,8 @@ public class Main {
 
         byte[] decodedBytes = Base64.getDecoder().decode(encodedText);
         String decodedText = new String(decodedBytes);
-        System.out.println(decodedText);
-        scanner.close();
+        System.out.println("Base64 decoded:" + decodedText);
+        start();
     }
 
     static void quit(){
